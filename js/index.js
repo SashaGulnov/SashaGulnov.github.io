accce0506456473e999371d426eb2f8d95a3445e
SashaGulnov <kirya_kox@hotmail.com>
2021-04-16 22:18:07 +0300

    added slick dots

---
+++ js/index.js
@@ -0,0 +1,29 @@
+btn = document.querySelector('.header__button');
+
+btn.addEventListener('click', function (){
+  alert('пока рано нажимать...')
+})
+
+$('.slider').slick({
+  infinite: true,
+  adaptiveHeight: true,
+  centerMode: false,
+  dots: true,
+})
+
+//Стилизация точек слайдера, на простом css нее работает почему-то
+
+document.querySelectorAll('.slick-dots li').forEach((el) => {
+  el.style.display = 'flex'
+  el.style.width = '100%'
+})
+
+tooltip = document.querySelectorAll('.tooltip')
+
+tooltip.forEach(function (el) {
+  el.addEventListener('click', function () {
+    this.querySelector('.tooltip__text').classList.toggle('active-tooltip-text')
+  }
+  )
+}
+)
