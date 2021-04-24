b4cb38810644708fc4416e873615a42d2d7c0278
Sasha Gulnov <kirya_kox@hotmail.com>
2021-04-24 17:43:40 +0300

    changed slider, added the second slider

--- js/index.js
+++ js/index.js
@@ -4,19 +4,30 @@ btn.addEventListener('click', function (){
   alert('пока рано нажимать...')
 })
 
-$('.main__slider').slick({
-  infinite: true,
-  adaptiveHeight: true,
-  centerMode: false,
-  dots: true,
+const slider = new Swiper ('.slider', {
+  loop: true, 
+  centeredSlides: true,
 })
 
-//Стилизация точек слайдера, на простом css нее работает почему-то
-
-document.querySelectorAll('.main__slider .slick-dots li').forEach((el) => {
-  el.style.display = 'flex'
-  el.style.width = '100%'
+slider.on('slideChange', function(){
+  room = document.querySelector('.rooms .active');
+  room.classList.remove('active');
+  room = document.querySelector(`.rooms`).children.item(slider.realIndex);
+  room.classList.toggle('active')
 })
+
+
+let rooms = document.querySelectorAll('.rooms .room')
+rooms.forEach((room)=>{
+    room.addEventListener('click', function() {
+      let arr = Array.prototype.slice.call(rooms)
+      index = arr.indexOf(room)
+      activeRoom = document.querySelector('.rooms .active');
+      activeRoom.classList.remove('active');
+      rooms.item(index).classList.toggle('active')
+      slider.slideTo(index+1)
+    })
+  })
 
 tooltip = document.querySelectorAll('.tooltip')
 
@@ -27,3 +38,65 @@ tooltip.forEach(function (el) {
   )
 }
 )
+
+const reviews = new Swiper('.reviews__slider', 
+{
+  init: false,
+  loop: true,
+  centeredSlides: true,
+  effect: 'coverflow',
+  slidesPerView: 'auto',
+  navigation :{
+    nextEl: '.reviews__next',
+    prevEl: '.reviews__prev'
+  },
+  coverflowEffect : {
+    rotate: 0,
+    stretch: "75%",
+    depth: 500,
+    slideShadows:false,
+    },
+}
+);
+
+reviews.on('init', function(){
+  let visibleSlides = [
+      reviews.slides[reviews.activeIndex-2],
+      reviews.slides[reviews.activeIndex-1],
+      reviews.slides[reviews.activeIndex],
+      reviews.slides[reviews.activeIndex+1],
+      reviews.slides[reviews.activeIndex+2]
+  ]
+ visibleSlides.forEach((e)=>{e.style.visibility='visible'});
+ checkArrows()
+})
+
+reviews.on('slideChange', function(){
+  reviews.slides.forEach((e)=>{e.style.visibility='hidden'})
+  let visibleSlides = [
+      reviews.slides[reviews.activeIndex-2],
+      reviews.slides[reviews.activeIndex-1],
+      reviews.slides[reviews.activeIndex],
+      reviews.slides[reviews.activeIndex+1],
+      reviews.slides[reviews.activeIndex+2]
+  ]
+  visibleSlides.forEach((e)=>{
+    e.style.visibility='visible'})
+})
+
+reviews.on('resize', checkArrows)
+
+reviews.init()
+
+function checkArrows() {
+  let nextButton = document.querySelector('.swiper-button-next.reviews__next')
+  let prevButton = document.querySelector('.swiper-button-prev.reviews__prev')
+  if (window.innerWidth<=960) {
+    nextButton.style.display = 'none';
+    prevButton.style.display = 'none';
+  }
+  else {
+    nextButton.style.display = 'flex';
+    prevButton.style.display = 'flex';
+  }
+}
