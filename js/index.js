b2ca2232afc31556668fa889c01dd71195e7f558
SashaGulnov <kirya_kox@hotmail.com>
2021-05-09 12:52:24 +0300

    PHPMailer, formValidation, numberMask

--- js/index.js
+++ js/index.js
@@ -1,102 +1,216 @@
-﻿btn = document.querySelector('.header__button');
+﻿'use strict'
 
-btn.addEventListener('click', function (){
-  alert('пока рано хуй сосать...')
-})
+let form = document.querySelector('.order-form__form')
+form.addEventListener('submit', formSend)
+let input_tel = form.querySelector('input[type=tel')
+input_tel.addEventListener("input", mask, false);
+input_tel.addEventListener("focus", mask, false);
+input_tel.addEventListener("blur", mask, false);
 
-const slider = new Swiper ('.slider', {
-  loop: true, 
-  centeredSlides: true,
-})
+  async function formSend(e) {
+    e.preventDefault();
+    let error = formValidate(form);
+    let formData = new FormData(form)
+    if (error === 0) {
+      form.classList.add('_sending')
+      let response = await fetch('sendmail.php',
+        {
+          method: 'POST',
+          body: formData,
+        })
+      if (response.ok) {
+        let result = await response.json()
+        Swal.fire(`Спасибо за заявку, ${result.message}`)
+        form.reset()
+        form.classList.remove('_sending')
+      } else {
+        Swal.fire('Ошибка')
+        form.classList.remove('_sending')
+      }
+    }
+  }
+  
+  function formValidate(form) {
+    let error = 0
+    let formReq = form.querySelectorAll('._req')
+  
+    for (let input of formReq) {
+      formRemoveError(input)
+      if (!input.value) {
+        formAddError(input)
+        error++
+      }
+      
+    }
+    return error
+}
 
-slider.on('slideChange', function(){
-  room = document.querySelector('.rooms .active');
-  room.classList.remove('active');
-  room = document.querySelector(`.rooms`).children.item(slider.realIndex);
-  room.classList.toggle('active')
-})
+//Функции валидации - маски номера телефона
+
+function setCursorPosition(pos, elem) {
+      elem.focus();
+      if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+      else if (elem.createTextRange) {
+          var range = elem.createTextRange();
+          range.collapse(true);
+          range.moveEnd("character", pos);
+          range.moveStart("character", pos);
+          range.select()
+      }
+  }
+   
+  function mask(event) {
+      var matrix = "+7 (___) ___ ____",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, "");
+      if (def.length >= val.length) val = def;
+      this.value = matrix.replace(/./g, function(a) {
+          return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+      });
+      if (event.type == "blur") {
+          if (this.value.length == 2) this.value = ""
+      } else setCursorPosition(this.value.length, this)
+  };
+  
+  function formAddError(input) {
+    input.classList.add('_error')
+  }
+  
+  function formRemoveError(input) {
+    input.classList.remove('_error')
+  }
 
 
-let rooms = document.querySelectorAll('.rooms .room')
-rooms.forEach((room)=>{
-    room.addEventListener('click', function() {
+  document.querySelectorAll('.button:not(.order-form__button)').forEach(
+    function (button) {
+      button.addEventListener('click', sweet)
+    })
+
+  function sweet() {
+    Swal.fire({
+      didOpen: () => {
+        form = document.querySelector('.swal2-popup .order-form__form')
+        form.addEventListener('submit', formSend)
+        input_tel = form.querySelector('input[type=tel')
+        input_tel.addEventListener("input", mask, false);
+        input_tel.addEventListener("focus", mask, false);
+        input_tel.addEventListener("blur", mask, false);
+      },
+      title: false,
+      showCloseButton: true,
+      showConfirmButton: false,
+      imageUrl: './img/icons/cleaningIconBlack.png',
+      html:
+        `<form action="#" class="order-form__form">` +
+        `<div class="order-form__title title">Заявка на расчет стоимости</div>` +
+        `<div class="order-form__inputs">` +
+        `<input type="text" name="name" placeholder="Ваше имя"  class="order-form__name _req">` +
+        `<input type="tel" name="number" placeholder="Ваш номер телефона" class="order-form__number _req">` +
+        `</div>` +
+        `<button class="order-form__button button"><span>Заказать</span></button>` +
+        `</form>`,
+      allowOutsideClick: () => !Swal.isLoading(),
+      didDestroy: () => {
+        form = document.querySelector('.order-form__form')
+      }
+    })
+  }
+
+  const slider = new Swiper('.slider', {
+    loop: true,
+    centeredSlides: true,
+  })
+
+  slider.on('slideChange', function () {
+    let room = document.querySelector('.rooms .active');
+    room.classList.remove('active');
+    room = document.querySelector(`.rooms`).children.item(slider.realIndex);
+    room.classList.toggle('active')
+  })
+
+
+  let rooms = document.querySelectorAll('.rooms .room')
+  rooms.forEach((room) => {
+    room.addEventListener('click', function () {
       let arr = Array.prototype.slice.call(rooms)
-      index = arr.indexOf(room)
-      activeRoom = document.querySelector('.rooms .active');
+      let index = arr.indexOf(room)
+      let activeRoom = document.querySelector('.rooms .active');
       activeRoom.classList.remove('active');
       rooms.item(index).classList.toggle('active')
-      slider.slideTo(index+1)
+      slider.slideTo(index + 1)
     })
   })
 
-tooltip = document.querySelectorAll('.tooltip')
+  let tooltip = document.querySelectorAll('.tooltip')
 
-tooltip.forEach(function (el) {
-  el.addEventListener('click', function () {
-    this.querySelector('.tooltip__text').classList.toggle('active-tooltip-text')
+  tooltip.forEach(function (el) {
+    el.addEventListener('click', function () {
+      this.querySelector('.tooltip__text').classList.toggle('active-tooltip-text')
+    }
+    )
   }
   )
-}
-)
 
-const reviews = new Swiper('.reviews__slider', 
-{
-  init: false,
-  loop: true,
-  centeredSlides: true,
-  effect: 'coverflow',
-  slidesPerView: 'auto',
-  navigation :{
-    nextEl: '.reviews__next',
-    prevEl: '.reviews__prev'
-  },
-  coverflowEffect : {
-    rotate: 0,
-    stretch: "75%",
-    depth: 500,
-    slideShadows:false,
-    },
-}
-);
+  const reviews = new Swiper('.reviews__slider',
+    {
+      init: false,
+      loop: true,
+      centeredSlides: true,
+      effect: 'coverflow',
+      slidesPerView: 'auto',
+      navigation: {
+        nextEl: '.reviews__next',
+        prevEl: '.reviews__prev'
+      },
+      coverflowEffect: {
+        rotate: 0,
+        stretch: "75%",
+        depth: 500,
+        slideShadows: false,
+      },
+    }
+  );
 
-reviews.on('init', function(){
-  let visibleSlides = [
-      reviews.slides[reviews.activeIndex-2],
-      reviews.slides[reviews.activeIndex-1],
+  reviews.on('init', function () {
+    let visibleSlides = [
+      reviews.slides[reviews.activeIndex - 2],
+      reviews.slides[reviews.activeIndex - 1],
       reviews.slides[reviews.activeIndex],
-      reviews.slides[reviews.activeIndex+1],
-      reviews.slides[reviews.activeIndex+2]
-  ]
- visibleSlides.forEach((e)=>{e.style.visibility='visible'});
- checkArrows()
-})
+      reviews.slides[reviews.activeIndex + 1],
+      reviews.slides[reviews.activeIndex + 2]
+    ]
+    visibleSlides.forEach((e) => { e.style.visibility = 'visible' });
+    checkArrows()
+  })
 
-reviews.on('slideChange', function(){
-  reviews.slides.forEach((e)=>{e.style.visibility='hidden'})
-  let visibleSlides = [
-      reviews.slides[reviews.activeIndex-2],
-      reviews.slides[reviews.activeIndex-1],
+  reviews.on('slideChange', function () {
+    reviews.slides.forEach((e) => { e.style.visibility = 'hidden' })
+    let visibleSlides = [
+      reviews.slides[reviews.activeIndex - 2],
+      reviews.slides[reviews.activeIndex - 1],
       reviews.slides[reviews.activeIndex],
-      reviews.slides[reviews.activeIndex+1],
-      reviews.slides[reviews.activeIndex+2]
-  ]
-  visibleSlides.forEach((e)=>{
-    e.style.visibility='visible'})
-})
+      reviews.slides[reviews.activeIndex + 1],
+      reviews.slides[reviews.activeIndex + 2]
+    ]
+    visibleSlides.forEach((e) => {
+      e.style.visibility = 'visible'
+    })
+  })
 
-reviews.on('resize', checkArrows)
+  reviews.on('resize', checkArrows)
 
-reviews.init()
+  reviews.init()
 
-function checkArrows() {
-  let nextButton = document.querySelector('.swiper-button-next.reviews__next')
-  let prevButton = document.querySelector('.swiper-button-prev.reviews__prev')
-  if (window.innerWidth<=960) {
-    nextButton.style.display = 'none';
-    prevButton.style.display = 'none';
+  function checkArrows() {
+    let nextButton = document.querySelector('.swiper-button-next.reviews__next')
+    let prevButton = document.querySelector('.swiper-button-prev.reviews__prev')
+    if (window.innerWidth <= 960) {
+      nextButton.style.display = 'none';
+      prevButton.style.display = 'none';
+    }
+    else {
+      nextButton.style.display = 'flex';
+      prevButton.style.display = 'flex';
+    }
   }
-  else {
-    nextButton.style.display = 'flex';
-    prevButton.style.display = 'flex';
-  }
-}
---
+++ sendmail.php
@@ -0,0 +1,66 @@
+<?php
+
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\Exception;
+   use PHPMailer\PHPMailer\SMTP;
+
+   require './PHPMailer/src/Exception.php';
+   require './PHPMailer/src/PHPMailer.php';
+   require 'PHPMailer/src/SMTP.php';
+
+   $mail = new PHPMailer(true);
+   $mail -> CharSet = 'UTF-8';
+   $mail->isSMTP();
+   $mail->SMTPAuth   = true;
+   $mail -> setLanguage('ru', './PHPMailer/language/');
+   $mail -> isHTML(true);
+
+
+
+   $mail->Host = 'ssl://smtp.mail.ru';
+   $mail->Username   = 'kirill_titarenko98@mail.ru'; // Логин на почте
+   $mail->Password   = 'pgyu7Dn1wpNyK2jleVeU'; // Пароль на почте
+   $mail->SMTPSecure = 'ssl';
+   // $mail->SMTPDebug = 2;
+   $mail->Port       = 465;
+
+
+   $mail -> setFrom('kirill_titarenko98@mail.ru', 'Кирилл');
+   $mail -> addAddress('kurkuedova.na@icloud.com');
+   $mail -> Subject = 'Hey!';
+
+
+
+
+
+   $body = '<h1>НАЧАЛО</h1>';
+
+   if(trim(!empty($_POST['name']))) {
+      $body.='<p>Имя: '.$_POST['name'].'</p>';
+   }
+
+   if(trim(!empty($_POST['number']))) {
+      $body.='<p>Номер: '.$_POST['number'].'</p>';
+   }
+
+   $mail -> Body = $body;
+
+
+   if (!$mail->send()) {
+      $message = 'Error';
+   } else {
+      $message = $_POST['name'];
+   }
+
+   
+
+   $response = array('message'=>$message);
+
+   echo json_encode($response);
+//    $name = $_POST['name'];
+//    $number = $_POST['number'];
+//    $response = array('name'=>$name);
+//    echo json_encode($response);
+
+
+?>
